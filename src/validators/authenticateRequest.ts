a7e4c5daa2a513caba0514abdb03359c7eee9416
joseFelipe7 <josefelipedas.santos@gmail.com>
2024-01-01 23:50:08 -0300

    fix(auth) removendo o name de campo obrigatorio

--- src/validators/authenticateRequest.ts
+++ src/validators/authenticateRequest.ts
@@ -1,7 +1,6 @@
 import joi from "joi"
 
 export default joi.object().keys({
-                    name:joi.string().trim().min(3).max(100).required(),
                     password:joi.string().min(3).max(100).required(),
                     email:joi.string().min(6).max(150).required().email(),
                 })
